c7bd49b63b34b144404cec43c5549a64bc8a0404
Jacobo Ossa <jaco2419@gmail.com>
2025-11-25 22:58:25 -0500

    feat: add usecases for owner can manage requests

---
+++ lib/features/offers/domain/usecases/approve_rental_request_usecase.dart
@@ -0,0 +1,18 @@
+import 'package:lendly_app/domain/model/rental_request.dart';
+import 'package:lendly_app/features/offers/data/repositories/rental_request_repository_impl.dart';
+import 'package:lendly_app/features/offers/data/source/rental_request_data_source.dart';
+import 'package:lendly_app/features/offers/domain/repositories/rental_request_repository.dart';
+
+class ApproveRentalRequestUseCase {
+  final RentalRequestRepository repository = RentalRequestRepositoryImpl(
+    RentalRequestDataSourceImpl(),
+  );
+
+  Future<RentalRequest> execute(String requestId) {
+    return repository.updateRentalRequestStatus(
+      requestId,
+      RentalRequestStatus.approved,
+    );
+  }
+}
+
